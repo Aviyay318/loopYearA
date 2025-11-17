0e4cec68aa7e9d11a79223ba9dfb5a5dfd3de120
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2025-11-17 16:24:49 +0200

    add break and continue

--- src/EX.java
+++ src/EX.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class EX {
     public static void main(String[] args) {
         int number = 16;
@@ -5,5 +7,18 @@ public class EX {
         double x = Math.pow(2,5);//חזקה
         int y = Math.abs(-4); //ערך מוחלט
         System.out.println(Math.sqrt(number));
+        Scanner scanner =new Scanner(System.in);
+
+        for (int i=0; i<5;i++){
+            int num = scanner.nextInt();;
+            if (num<0){
+                System.out.println("error");
+                break; // מסיים את הלולאה
+            }
+            if (num>100){
+                continue; //מדלג על איטרציה
+            }
+            System.out.println(num);
+        }
     }
 }
