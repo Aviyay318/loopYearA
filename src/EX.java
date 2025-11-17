52d1f205e847466554049647d2edf82b0ed0c548
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2025-11-17 16:16:40 +0200

    add  EX14 +EX15

---
+++ src/EX.java
@@ -0,0 +1,9 @@
+public class EX {
+    public static void main(String[] args) {
+        int number = 16;
+        double s = Math.sqrt(number); //שורש
+        double x = Math.pow(2,5);//חזקה
+        int y = Math.abs(-4); //ערך מוחלט
+        System.out.println(Math.sqrt(number));
+    }
+}
--- src/Ex15.java
+++ src/Ex15.java
@@ -8,5 +8,13 @@ public class Ex15 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int lines = scanner.nextInt();
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < lines; j++) {
+                System.out.print(lines);
+            }
+            System.out.println();
+        }
     }
 }
